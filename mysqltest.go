226b9d6dafc4533050ed636527650b36b0648b67
Naitik Shah <n@daaku.org>
2013-12-03 22:14:30 -0800

    use waitout instead of trying to connecting

--- mysqltest.go
+++ mysqltest.go
@@ -1,23 +1,27 @@
-// Package mysqltest provides standalone instances of mysql sutable for use in
-// tests.
+// Package mysqltest provides standalone test instances of mysql sutable for
+// use in tests.
 package mysqltest
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
+	"regexp"
 	"text/template"
-	"time"
 
 	// We're optionally provide a DB instance backed by this driver.
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/ParsePlatform/go.freeport"
+	"github.com/ParsePlatform/go.waitout"
 )
+
+var mysqlReadyForConnections = []byte("mysqld: ready for connections")
 
 var configTemplate, configTemplateErr = template.New("config").Parse(`
 [mysqld]
@@ -37,15 +41,25 @@ port                            = {{.Port}}
 socket                          = {{.Socket}}
 sort_buffer_size                = 32K
 sql_mode                        = ''
-table_cache                     = 2
 thread_cache_size               = 2
 thread_stack                    = 128K
 `)
+
+var mysqlBaseDir string
 
 func init() {
 	if configTemplateErr != nil {
 		panic(configTemplateErr)
 	}
+
+	out, err := exec.Command("mysqld", "--help", "--verbose").CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+
+	// The spaces are important.
+	hit := regexp.MustCompile(`basedir        .*`).Find(out)
+	mysqlBaseDir = string(bytes.TrimSpace(hit[8:]))
 }
 
 // Fatalf is satisfied by testing.T or testing.B.
@@ -81,25 +95,29 @@ func (s *Server) Start() {
 	if err != nil {
 		s.T.Fatalf(err.Error())
 	}
-
 	if err := configTemplate.Execute(cf, s); err != nil {
+		s.T.Fatalf(err.Error())
+	}
+	if err := cf.Close(); err != nil {
 		s.T.Fatalf(err.Error())
 	}
 
 	defaultsFile := fmt.Sprintf("--defaults-file=%s", cf.Name())
-	s.cmd = exec.Command("mysql_install_db", defaultsFile)
+	s.cmd = exec.Command("mysql_install_db", defaultsFile, "--basedir", mysqlBaseDir)
 	s.cmd.Stdout = os.Stdout
 	s.cmd.Stderr = os.Stderr
 	if err := s.cmd.Run(); err != nil {
 		s.T.Fatalf(err.Error())
 	}
 
-	s.cmd = exec.Command("mysqld", defaultsFile)
+	waiter := waitout.New(mysqlReadyForConnections)
+	s.cmd = exec.Command("mysqld", defaultsFile, "--basedir", mysqlBaseDir)
 	s.cmd.Stdout = os.Stdout
-	s.cmd.Stderr = os.Stderr
+	s.cmd.Stderr = io.MultiWriter(os.Stderr, waiter)
 	if err := s.cmd.Start(); err != nil {
 		s.T.Fatalf(err.Error())
 	}
+	waiter.Wait()
 }
 
 // Stop the server, this will also remove all data.
@@ -116,23 +134,11 @@ func (s *Server) DSN(suffix string) string {
 
 // DB for the server. The suffix is in the form "dbname?param=value".
 func (s *Server) DB(suffix string) *sql.DB {
-	for {
-		db, err := sql.Open("mysql", s.DSN(suffix))
-		if err != nil {
-			s.T.Fatalf(err.Error())
-		}
-
-		// Note the comment /* ping */ is important.
-		const pingQuery = "/* ping */ SELECT 1"
-		_, err = db.Exec(pingQuery)
-		if err == nil {
-			return db
-		}
-		if !strings.HasSuffix(err.Error(), "connection refused") {
-			s.T.Fatalf(err.Error())
-		}
-		time.Sleep(2 * time.Millisecond)
+	db, err := sql.Open("mysql", s.DSN(suffix))
+	if err != nil {
+		s.T.Fatalf(err.Error())
 	}
+	return db
 }
 
 // NewStartedServer creates a new server starts it.
